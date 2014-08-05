69bf80c3596adb6e336d6cc4f1b1503a65454703
ad-riel <ad55tan@gmail.com>
2014-08-05 09:13:46 +0000

    Assignment3 updated

--- bookmark/views.py
+++ bookmark/views.py
@@ -113,7 +113,7 @@ def add_page(request, category_name_url):
             
             # Now call the index() view.
             # The user will be shown the homepage.
-            return index(request)
+            return category(request, category_name_url)
         else:
             # The supplied form contained errors - just print them to the terminal.
             print form.errors
